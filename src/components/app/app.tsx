238a2e3c5f3f480b4aad21e6bac83abccaa545e8
Vladislav Guzenkov <vladislav.srg@yandex.ru>
2024-07-06 09:17:29 +0300

    Добавлена маршрутизация

--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -1,5 +1,15 @@
 import { Routes, Route } from 'react-router-dom';
-import { ConstructorPage } from '@pages';
+import {
+  ConstructorPage,
+  Feed,
+  Login,
+  Register,
+  ForgotPassword,
+  ResetPassword,
+  Profile,
+  ProfileOrders,
+  NotFound404
+} from '@pages';
 import '../../index.css';
 import styles from './app.module.css';
 
@@ -8,7 +18,17 @@ import { AppHeader } from '@components';
 const App = () => (
   <div className={styles.app}>
     <AppHeader />
-    <ConstructorPage />
+    <Routes>
+      <Route path='/' element={<ConstructorPage />} />
+      <Route path='/feed' element={<Feed />} />
+      <Route path='/login' element={<Login />} />
+      <Route path='/register' element={<Register />} />
+      <Route path='/forgot-password' element={<ForgotPassword />} />
+      <Route path='/reset-password' element={<ResetPassword />} />
+      <Route path='/profile' element={<Profile />} />
+      <Route path='/profile/orders' element={<ProfileOrders />} />
+      <Route path='/profile/orders' element={<NotFound404 />} />
+    </Routes>
   </div>
 );
 
