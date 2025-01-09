28059566d84c97b84decc0f2b79199a2f70067fb
Vladislav Guzenkov <vladislav.srg@yandex.ru>
2025-01-09 22:04:25 +0300

    Добавлены данные количества заказов

--- src/components/feed-info/feed-info.tsx
+++ src/components/feed-info/feed-info.tsx
@@ -2,6 +2,8 @@ import { FC } from 'react';
 
 import { TOrder } from '@utils-types';
 import { FeedInfoUI } from '../ui/feed-info';
+import { useSelector } from '../../services/store';
+import { selectorFeed } from '../../services/feedSlice';
 
 const getOrders = (orders: TOrder[], status: string): number[] =>
   orders
@@ -10,9 +12,14 @@ const getOrders = (orders: TOrder[], status: string): number[] =>
     .slice(0, 20);
 
 export const FeedInfo: FC = () => {
+  const { getFeedInfo } = selectorFeed;
+
   /** TODO: взять переменные из стора */
-  const orders: TOrder[] = [];
-  const feed = {};
+  const orders: TOrder[] = useSelector(getFeedInfo);
+  const feed = {
+    total: orders.length,
+    totalToday: orders.filter(order => new Date(order.createdAt).toDateString() === new Date().toDateString()).length
+  }
 
   const readyOrders = getOrders(orders, 'done');
 
