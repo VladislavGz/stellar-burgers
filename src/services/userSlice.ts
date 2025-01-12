b10f9359a1244fc4b69a2e60e9d22886b6da6d6e
Vladislav Guzenkov <vladislav.srg@yandex.ru>
2025-01-12 21:53:42 +0300

    Добавлены данные для регистрации

--- src/services/userSlice.ts
+++ src/services/userSlice.ts
@@ -1,4 +1,4 @@
-import { getUserApi, loginUserApi } from "@api";
+import { getUserApi, loginUserApi, registerUserApi } from "@api";
 import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
 import { TUser } from "@utils-types"
 
@@ -8,12 +8,14 @@ type TUserState = {
     user: TUser | null;
     isAuthChecked: boolean;
     loginErrorMessage: string;
+    registerErrorMessage: string;
 }
 
 const initialState: TUserState = {
     user: null,
     isAuthChecked: false,
-    loginErrorMessage: ''
+    loginErrorMessage: '',
+    registerErrorMessage: ''
 }
 
 export const getUser = createAsyncThunk(
@@ -26,12 +28,18 @@ export const loginUser = createAsyncThunk(
     loginUserApi
 )
 
+export const registerUser = createAsyncThunk(
+    'user/registerUser',
+    registerUserApi
+)
+
 export const userSlice = createSlice({
     name: 'userSlice',
     initialState,
     reducers: {},
     extraReducers: (builder) => {
         builder
+            //проверка
             .addCase(getUser.fulfilled, (state, action) => {
                 state.user = action.payload.user;
                 state.isAuthChecked = true;
@@ -39,6 +47,8 @@ export const userSlice = createSlice({
             .addCase(getUser.rejected, (state) => {
                 state.isAuthChecked = true;
             })
+
+            //логин
             .addCase(loginUser.pending, (state) => {
                 state.loginErrorMessage = '';
             })
@@ -48,11 +58,24 @@ export const userSlice = createSlice({
             .addCase(loginUser.rejected, (state, error) => {
                 state.loginErrorMessage = error.error.message || '';
             })
+
+            //регистрация
+            .addCase(registerUser.pending, (state) => {
+                state.registerErrorMessage = '';
+            })
+            .addCase(registerUser.fulfilled, (state, action) => {
+                console.log(action.payload);
+            })
+            .addCase(registerUser.rejected, (state, error) => {
+                state.registerErrorMessage = error.error.message || '';
+            })
+
     },
     selectors: {
         selectorIsAuthChecked: (state: TUserState) => state.isAuthChecked,
         selectorUserData: (state: TUserState) => state.user,
-        selectorLoginErrorMessage: (state: TUserState) => state.loginErrorMessage
+        selectorLoginErrorMessage: (state: TUserState) => state.loginErrorMessage,
+        selectorRegisterErrorMessage: (state: TUserState) => state.registerErrorMessage,
     }
 });
 
