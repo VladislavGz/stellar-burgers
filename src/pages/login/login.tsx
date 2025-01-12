94e68bdd339410b9649441f2f6f2c136d16f56de
Vladislav Guzenkov <vladislav.srg@yandex.ru>
2025-01-12 21:42:30 +0300

    Добавлены обработка логина и строка ошибки логина

--- src/pages/login/login.tsx
+++ src/pages/login/login.tsx
@@ -1,17 +1,31 @@
 import { FC, SyntheticEvent, useState } from 'react';
 import { LoginUI } from '@ui-pages';
+import { useDispatch, useSelector } from '../../services/store';
+import { loginUser, selectorUser } from '../../services/userSlice';
+import { TLoginData } from '@api';
 
 export const Login: FC = () => {
+  const dispatch = useDispatch();
+  const {selectorLoginErrorMessage} = selectorUser;
+
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
 
+  const loginData: TLoginData = {
+    email, password
+  }
+
+  const loginErrorMessage = useSelector(selectorLoginErrorMessage);
+  
+
   const handleSubmit = (e: SyntheticEvent) => {
     e.preventDefault();
+    dispatch(loginUser(loginData));
   };
 
   return (
     <LoginUI
-      errorText=''
+      errorText={loginErrorMessage}
       email={email}
       setEmail={setEmail}
       password={password}
