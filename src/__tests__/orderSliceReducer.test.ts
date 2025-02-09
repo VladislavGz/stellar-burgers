c61c364a0bbf7b312cb2a3ae912766d72d8d51db
Vladislav Guzenkov <vladislav.srg@yandex.ru>
2025-02-09 16:58:53 +0300

    добавлены тесты для orderSlice reducer

---
+++ src/__tests__/orderSliceReducer.test.ts
@@ -0,0 +1,44 @@
+import { RequestStatus, TOrder } from "@utils-types";
+import { orderSlice, initialState, getOrderByNumber } from "../../src/services/orderSlice";
+
+const mockOrders: TOrder[] = [
+    {
+        _id: 'order_1',
+        status: '',
+        name: '',
+        createdAt: '',
+        updatedAt: '',
+        number: 0,
+        ingredients: ['1', '1']
+    }
+];
+
+describe('orderSlice reducer', () => {
+    test('initialization correct', () => {
+        const state = orderSlice.reducer(undefined, { type: '' });
+        expect(state).toEqual(initialState);
+    });
+
+    test('getOrderByNumber pending', () => {
+        const action = { type: getOrderByNumber.pending.type };
+        const state = orderSlice.reducer(initialState, action);
+        expect(state.requestStatus).toBe(RequestStatus.Loading);
+    });
+
+    test('getOrderByNumber fulfilled', () => {
+        const action = {
+            type: getOrderByNumber.fulfilled.type, payload: {
+                orders: mockOrders
+            }
+        };
+        const state = orderSlice.reducer(initialState, action);
+        expect(state.requestStatus).toBe(RequestStatus.Success);
+        expect(state.order).toEqual(mockOrders);
+    });
+
+    test('getOrderByNumber rejected', () => {
+        const action = { type: getOrderByNumber.rejected.type };
+        const state = orderSlice.reducer(initialState, action);
+        expect(state.requestStatus).toBe(RequestStatus.Failed);
+    });
+});
