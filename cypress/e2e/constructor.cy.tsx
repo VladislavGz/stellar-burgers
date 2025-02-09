b455e92bbe9eb9d2007cad1a39cde46b857396bd
Vladislav Guzenkov <vladislav.srg@yandex.ru>
2025-02-10 00:15:29 +0300

    Добавлены тесты для добавления ингредиентов в конструктор

--- cypress/e2e/constructor.cy.tsx
+++ cypress/e2e/constructor.cy.tsx
@@ -1,8 +1,24 @@
-import type {} from 'cypress';
+import type { } from 'cypress';
 
 describe('Application', () => {
+
     beforeEach(() => {
         cy.viewport(1920, 1080);
+
+        cy.intercept('GET', 'ingredients', { fixture: 'ingredients' });
+        cy.intercept('GET', 'user', { fixture: 'user' });
+
         cy.visit('http://localhost:5173/');
+    });
+
+    it('adding an ingredient to the constructor', () => {
+        const testIngredientsId = ['643d69a5c3f7b9001cfa0941', '643d69a5c3f7b9001cfa093c'];
+
+        testIngredientsId.forEach(id => {
+            cy.get(`[data-testid=cy_${id}] > button`).click();
+            const burgerConstructor = cy.get('[data-testid=cy_burgerConstructor]');
+            const item = burgerConstructor.find(`[data-testid=cy_${id}]`);
+            item.should('be.visible');
+        });
     });
 });
