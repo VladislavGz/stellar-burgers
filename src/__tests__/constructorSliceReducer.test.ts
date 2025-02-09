41b078cdf6f07ea1b4b9c418bc020d70479b7da7
Vladislav Guzenkov <vladislav.srg@yandex.ru>
2025-02-09 20:12:22 +0300

    lint fix

--- src/__tests__/constructorSliceReducer.test.ts
+++ src/__tests__/constructorSliceReducer.test.ts
@@ -1,11 +1,109 @@
-import { RequestStatus, TConstructorIngredient, TOrder } from "@utils-types";
-import { TConstructorState } from "../../src/services/constructorSlice";
-import { initialState, constructorSlice, constructorActions, getNewOrder } from "../../src/services/constructorSlice";
+import { RequestStatus, TConstructorIngredient, TOrder } from '@utils-types';
+import { TConstructorState } from '../../src/services/constructorSlice';
+import {
+  initialState,
+  constructorSlice,
+  constructorActions,
+  getNewOrder
+} from '../../src/services/constructorSlice';
 
 const { addItem, removeItem, moveDown, moveUp } = constructorActions;
 
 const mockIngredients: TConstructorIngredient[] = [
-    {
+  {
+    id: 'test_0',
+    _id: 'test_0',
+    name: 'name_0',
+    type: '',
+    proteins: 0,
+    fat: 0,
+    carbohydrates: 0,
+    calories: 0,
+    price: 0,
+    image: '',
+    image_large: '',
+    image_mobile: ''
+  },
+
+  {
+    id: 'test_1',
+    _id: 'test_1',
+    name: 'name_1',
+    type: '',
+    proteins: 0,
+    fat: 0,
+    carbohydrates: 0,
+    calories: 0,
+    price: 0,
+    image: '',
+    image_large: '',
+    image_mobile: ''
+  },
+
+  {
+    id: 'test_2',
+    _id: 'test_2',
+    name: 'name_2',
+    type: '',
+    proteins: 0,
+    fat: 0,
+    carbohydrates: 0,
+    calories: 0,
+    price: 0,
+    image: '',
+    image_large: '',
+    image_mobile: ''
+  }
+];
+
+const mockOrder: TOrder = {
+  _id: 'new_order_id',
+  status: '',
+  name: '',
+  createdAt: '',
+  updatedAt: '',
+  number: 0,
+  ingredients: ['1', '2']
+};
+
+const mockState: TConstructorState = {
+  ...initialState,
+  items: { bun: null, ingredients: mockIngredients }
+};
+
+describe('constructorSlice reducer', () => {
+  test('initialization correct', () => {
+    const state = constructorSlice.reducer(undefined, { type: '' });
+    expect(state).toEqual(initialState);
+  });
+
+  test('add ingredient', () => {
+    const newIngredient: TConstructorIngredient = {
+      id: 'test_3',
+      _id: 'test_3',
+      name: 'name_3',
+      type: '',
+      proteins: 0,
+      fat: 0,
+      carbohydrates: 0,
+      calories: 0,
+      price: 0,
+      image: '',
+      image_large: '',
+      image_mobile: ''
+    };
+
+    const state = constructorSlice.reducer(mockState, addItem(newIngredient));
+    const ingredients = state.items.ingredients;
+    const lastIngredient = ingredients[ingredients.length - 1];
+
+    expect(ingredients.length).toBe(mockIngredients.length + 1);
+    expect(lastIngredient).toEqual(newIngredient);
+  });
+
+  test('remove ingredient', () => {
+    const testIngredients = [
+      {
         id: 'test_0',
         _id: 'test_0',
         name: 'name_0',
@@ -17,25 +115,10 @@ const mockIngredients: TConstructorIngredient[] = [
         price: 0,
         image: '',
         image_large: '',
-        image_mobile: '',
-    },
+        image_mobile: ''
+      },
 
-    {
-        id: 'test_1',
-        _id: 'test_1',
-        name: 'name_1',
-        type: '',
-        proteins: 0,
-        fat: 0,
-        carbohydrates: 0,
-        calories: 0,
-        price: 0,
-        image: '',
-        image_large: '',
-        image_mobile: '',
-    },
-
-    {
+      {
         id: 'test_2',
         _id: 'test_2',
         name: 'name_2',
@@ -47,224 +130,155 @@ const mockIngredients: TConstructorIngredient[] = [
         price: 0,
         image: '',
         image_large: '',
-        image_mobile: '',
-    }
-];
+        image_mobile: ''
+      }
+    ];
 
-const mockOrder: TOrder = {
-    _id: 'new_order_id',
-    status: '',
-    name: '',
-    createdAt: '',
-    updatedAt: '',
-    number: 0,
-    ingredients: ['1', '2']
-};
+    const state = constructorSlice.reducer(
+      mockState,
+      removeItem({ id: 'test_1' })
+    );
+    const ingredients = state.items.ingredients;
 
-const mockState: TConstructorState = { ...initialState, items: { bun: null, ingredients: mockIngredients } };
+    expect(ingredients.length).toBe(mockIngredients.length - 1);
+    expect(ingredients).toEqual(testIngredients);
+  });
 
-describe('constructorSlice reducer', () => {
-    test('initialization correct', () => {
-        const state = constructorSlice.reducer(undefined, { type: '' });
-        expect(state).toEqual(initialState);
-    });
+  test('moveDown ingredient', () => {
+    const testIngredients = [
+      {
+        id: 'test_0',
+        _id: 'test_0',
+        name: 'name_0',
+        type: '',
+        proteins: 0,
+        fat: 0,
+        carbohydrates: 0,
+        calories: 0,
+        price: 0,
+        image: '',
+        image_large: '',
+        image_mobile: ''
+      },
+      {
+        id: 'test_2',
+        _id: 'test_2',
+        name: 'name_2',
+        type: '',
+        proteins: 0,
+        fat: 0,
+        carbohydrates: 0,
+        calories: 0,
+        price: 0,
+        image: '',
+        image_large: '',
+        image_mobile: ''
+      },
+      {
+        id: 'test_1',
+        _id: 'test_1',
+        name: 'name_1',
+        type: '',
+        proteins: 0,
+        fat: 0,
+        carbohydrates: 0,
+        calories: 0,
+        price: 0,
+        image: '',
+        image_large: '',
+        image_mobile: ''
+      }
+    ];
 
-    test('add ingredient', () => {
-        const newIngredient: TConstructorIngredient = {
-            id: 'test_3',
-            _id: 'test_3',
-            name: 'name_3',
-            type: '',
-            proteins: 0,
-            fat: 0,
-            carbohydrates: 0,
-            calories: 0,
-            price: 0,
-            image: '',
-            image_large: '',
-            image_mobile: '',
-        };
+    const state = constructorSlice.reducer(mockState, moveDown(1));
+    const ingredients = state.items.ingredients;
 
-        const state = constructorSlice.reducer(mockState, addItem(newIngredient));
-        const ingredients = state.items.ingredients;
-        const lastIngredient = ingredients[ingredients.length - 1];
+    expect(ingredients.length).toBe(mockIngredients.length);
+    expect(ingredients).toEqual(testIngredients);
+  });
 
-        expect(ingredients.length).toBe(mockIngredients.length + 1);
-        expect(lastIngredient).toEqual(newIngredient);
-    });
+  test('moveUp ingredient', () => {
+    const testIngredients = [
+      {
+        id: 'test_1',
+        _id: 'test_1',
+        name: 'name_1',
+        type: '',
+        proteins: 0,
+        fat: 0,
+        carbohydrates: 0,
+        calories: 0,
+        price: 0,
+        image: '',
+        image_large: '',
+        image_mobile: ''
+      },
 
-    test('remove ingredient', () => {
-        const testIngredients = [
-            {
-                id: 'test_0',
-                _id: 'test_0',
-                name: 'name_0',
-                type: '',
-                proteins: 0,
-                fat: 0,
-                carbohydrates: 0,
-                calories: 0,
-                price: 0,
-                image: '',
-                image_large: '',
-                image_mobile: '',
-            },
+      {
+        id: 'test_0',
+        _id: 'test_0',
+        name: 'name_0',
+        type: '',
+        proteins: 0,
+        fat: 0,
+        carbohydrates: 0,
+        calories: 0,
+        price: 0,
+        image: '',
+        image_large: '',
+        image_mobile: ''
+      },
 
-            {
-                id: 'test_2',
-                _id: 'test_2',
-                name: 'name_2',
-                type: '',
-                proteins: 0,
-                fat: 0,
-                carbohydrates: 0,
-                calories: 0,
-                price: 0,
-                image: '',
-                image_large: '',
-                image_mobile: '',
-            }
-        ];
+      {
+        id: 'test_2',
+        _id: 'test_2',
+        name: 'name_2',
+        type: '',
+        proteins: 0,
+        fat: 0,
+        carbohydrates: 0,
+        calories: 0,
+        price: 0,
+        image: '',
+        image_large: '',
+        image_mobile: ''
+      }
+    ];
 
-        const state = constructorSlice.reducer(mockState, removeItem({ id: 'test_1' }));
-        const ingredients = state.items.ingredients;
+    const state = constructorSlice.reducer(mockState, moveUp(1));
+    const ingredients = state.items.ingredients;
 
-        expect(ingredients.length).toBe(mockIngredients.length - 1);
-        expect(ingredients).toEqual(testIngredients);
-    });
+    expect(ingredients.length).toBe(mockIngredients.length);
+    expect(ingredients).toEqual(testIngredients);
+  });
 
-    test('moveDown ingredient', () => {
-        const testIngredients = [
-            {
-                id: 'test_0',
-                _id: 'test_0',
-                name: 'name_0',
-                type: '',
-                proteins: 0,
-                fat: 0,
-                carbohydrates: 0,
-                calories: 0,
-                price: 0,
-                image: '',
-                image_large: '',
-                image_mobile: ''
-            },
-            {
-                id: 'test_2',
-                _id: 'test_2',
-                name: 'name_2',
-                type: '',
-                proteins: 0,
-                fat: 0,
-                carbohydrates: 0,
-                calories: 0,
-                price: 0,
-                image: '',
-                image_large: '',
-                image_mobile: ''
-            },
-            {
-                id: 'test_1',
-                _id: 'test_1',
-                name: 'name_1',
-                type: '',
-                proteins: 0,
-                fat: 0,
-                carbohydrates: 0,
-                calories: 0,
-                price: 0,
-                image: '',
-                image_large: '',
-                image_mobile: ''
-            }
-        ];
+  test('getNewOrder pending', () => {
+    const action = { type: getNewOrder.pending.type };
+    const state = constructorSlice.reducer(mockState, action);
 
-        const state = constructorSlice.reducer(mockState, moveDown(1));
-        const ingredients = state.items.ingredients;
+    expect(state.order.isOrderRequest).toBe(true);
+    expect(state.order.requestStatus).toBe(RequestStatus.Loading);
+  });
 
-        expect(ingredients.length).toBe(mockIngredients.length);
-        expect(ingredients).toEqual(testIngredients);
-    });
+  test('getNewOrder fulfilled', () => {
+    const action = {
+      type: getNewOrder.fulfilled.type,
+      payload: {
+        order: mockOrder
+      }
+    };
+    const state = constructorSlice.reducer(mockState, action);
 
-    test('moveUp ingredient', () => {
-        const testIngredients = [
-            {
-                id: 'test_1',
-                _id: 'test_1',
-                name: 'name_1',
-                type: '',
-                proteins: 0,
-                fat: 0,
-                carbohydrates: 0,
-                calories: 0,
-                price: 0,
-                image: '',
-                image_large: '',
-                image_mobile: ''
-            },
+    expect(state.order.isOrderRequest).toBe(true);
+    expect(state.order.requestStatus).toBe(RequestStatus.Success);
+    expect(state.order.orderData).toEqual(mockOrder);
+  });
 
-            {
-                id: 'test_0',
-                _id: 'test_0',
-                name: 'name_0',
-                type: '',
-                proteins: 0,
-                fat: 0,
-                carbohydrates: 0,
-                calories: 0,
-                price: 0,
-                image: '',
-                image_large: '',
-                image_mobile: ''
-            },
+  test('getNewOrder rejected', () => {
+    const action = { type: getNewOrder.rejected.type };
+    const state = constructorSlice.reducer(mockState, action);
 
-            {
-                id: 'test_2',
-                _id: 'test_2',
-                name: 'name_2',
-                type: '',
-                proteins: 0,
-                fat: 0,
-                carbohydrates: 0,
-                calories: 0,
-                price: 0,
-                image: '',
-                image_large: '',
-                image_mobile: ''
-            }
-        ];
-
-        const state = constructorSlice.reducer(mockState, moveUp(1));
-        const ingredients = state.items.ingredients;
-
-        expect(ingredients.length).toBe(mockIngredients.length);
-        expect(ingredients).toEqual(testIngredients);
-    });
-
-    test('getNewOrder pending', () => {
-        const action = { type: getNewOrder.pending.type };
-        const state = constructorSlice.reducer(mockState, action);
-
-        expect(state.order.isOrderRequest).toBe(true);
-        expect(state.order.requestStatus).toBe(RequestStatus.Loading);
-    });
-
-    test('getNewOrder fulfilled', () => {
-        const action = {type: getNewOrder.fulfilled.type, payload: {
-            order: mockOrder
-        }};
-        const state = constructorSlice.reducer(mockState, action);
-
-        expect(state.order.isOrderRequest).toBe(true);
-        expect(state.order.requestStatus).toBe(RequestStatus.Success);
-        expect(state.order.orderData).toEqual(mockOrder);
-    });
-
-    test('getNewOrder rejected', () => {
-        const action = { type: getNewOrder.rejected.type };
-        const state = constructorSlice.reducer(mockState, action);
-
-        expect(state.order.isOrderRequest).toBe(true);
-        expect(state.order.requestStatus).toBe(RequestStatus.Failed);
-    });
+    expect(state.order.isOrderRequest).toBe(true);
+    expect(state.order.requestStatus).toBe(RequestStatus.Failed);
+  });
 });
--- src/__tests__/feedSliceReducer.test.ts
+++ src/__tests__/feedSliceReducer.test.ts
@@ -1,54 +1,59 @@
-import { RequestStatus, TOrder } from "@utils-types";
-import { feedSlice, initialState, getFeeds } from "../../src/services/feedSlice";
+import { RequestStatus, TOrder } from '@utils-types';
+import {
+  feedSlice,
+  initialState,
+  getFeeds
+} from '../../src/services/feedSlice';
 
 const mockOrders: TOrder[] = [
-    {
-        _id: 'order_1',
-        status: '',
-        name: '',
-        createdAt: '',
-        updatedAt: '',
-        number: 0,
-        ingredients: ['1', '1']
-    },
+  {
+    _id: 'order_1',
+    status: '',
+    name: '',
+    createdAt: '',
+    updatedAt: '',
+    number: 0,
+    ingredients: ['1', '1']
+  },
 
-    {
-        _id: 'order_2',
-        status: '',
-        name: '',
-        createdAt: '',
-        updatedAt: '',
-        number: 0,
-        ingredients: ['1', '1']
-    }
+  {
+    _id: 'order_2',
+    status: '',
+    name: '',
+    createdAt: '',
+    updatedAt: '',
+    number: 0,
+    ingredients: ['1', '1']
+  }
 ];
 
 describe('feedSlice reducer', () => {
-    test('initialization correct', () => {
-        const state = feedSlice.reducer(undefined, { type: '' });
-        expect(state).toEqual(initialState);
-    });
+  test('initialization correct', () => {
+    const state = feedSlice.reducer(undefined, { type: '' });
+    expect(state).toEqual(initialState);
+  });
 
-    test('getFeeds pending', () => {
-        const action = { type: getFeeds.pending.type };
-        const state = feedSlice.reducer(initialState, action);
-        expect(state.requestStatus).toBe(RequestStatus.Loading);
-    });
+  test('getFeeds pending', () => {
+    const action = { type: getFeeds.pending.type };
+    const state = feedSlice.reducer(initialState, action);
+    expect(state.requestStatus).toBe(RequestStatus.Loading);
+  });
 
-    test('getFeeds fulfilled', () => {
-        const action = {
-            type: getFeeds.fulfilled.type, payload: {
-                orders: mockOrders
-            }
-        };
-        const state = feedSlice.reducer(initialState, action);
-        expect(state.requestStatus).toBe(RequestStatus.Success);
-        expect(state.orders).toEqual(mockOrders);
-    });
+  test('getFeeds fulfilled', () => {
+    const action = {
+      type: getFeeds.fulfilled.type,
+      payload: {
+        orders: mockOrders
+      }
+    };
+    const state = feedSlice.reducer(initialState, action);
+    expect(state.requestStatus).toBe(RequestStatus.Success);
+    expect(state.orders).toEqual(mockOrders);
+  });
 
-    test('getFeeds rejected', () => {
-        const action = { type: getFeeds.rejected.type };
-        const state = feedSlice.reducer(initialState, action);
-        expect(state.requestStatus).toBe(RequestStatus.Failed);
-    });
+  test('getFeeds rejected', () => {
+    const action = { type: getFeeds.rejected.type };
+    const state = feedSlice.reducer(initialState, action);
+    expect(state.requestStatus).toBe(RequestStatus.Failed);
+  });
 });
