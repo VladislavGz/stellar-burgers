51014d5f76c29d7f7a696cf964e545f52096563b
Vladislav Guzenkov <vladislav.srg@yandex.ru>
2025-02-10 22:37:46 +0300

    Добавлены комментарии

--- cypress/e2e/constructor.cy.tsx
+++ cypress/e2e/constructor.cy.tsx
@@ -5,6 +5,7 @@ describe('Application', () => {
     beforeEach(() => {
         cy.viewport(1920, 1080);
 
+        //перехват запросов
         cy.intercept('GET', 'ingredients', { fixture: 'ingredients' });
         cy.intercept('GET', 'user', { fixture: 'user' });
         cy.intercept('POST', 'orders', { fixture: 'orderRequest' })
@@ -12,6 +13,7 @@ describe('Application', () => {
         cy.visit('http://localhost:5173/');
     });
 
+    //проверка добавления ингредиентов в конструктор
     it('adding an ingredient to the constructor', () => {
         const testIngredientsId = ['643d69a5c3f7b9001cfa0941', '643d69a5c3f7b9001cfa093c'];
 
@@ -23,6 +25,7 @@ describe('Application', () => {
         });
     });
 
+    //проверка открытия модального окна ингредиента
     it('opening the ingredient modal', () => {
         const testIngredientId = '643d69a5c3f7b9001cfa0941';
         cy.get(`[data-testid=cy_${testIngredientId}]`).click();
@@ -34,6 +37,7 @@ describe('Application', () => {
         detailsElement.should('be.visible');
     });
 
+    //проверка закрытия модального окна ингредиента по клику на крестик
     it('closing the modal of the ingredient by clicking on the button', () => {
         const testIngredientId = '643d69a5c3f7b9001cfa093c';
         cy.get(`[data-testid=cy_${testIngredientId}]`).click();
@@ -45,6 +49,7 @@ describe('Application', () => {
         modal.should('not.exist');
     });
 
+    //проверка закрытия модального окна ингредиента по клику на оверлей
     it('closing the modal of an ingredient by clicking on the overlay', () => {
         const testIngredientId = '643d69a5c3f7b9001cfa093c';
         cy.get(`[data-testid=cy_${testIngredientId}]`).click();
@@ -57,25 +62,33 @@ describe('Application', () => {
         modal.should('not.exist');
     });
 
+    //проверка сборки бургера, оформления заказа, модального окна заказа и очистки конструктора
     it('assembling a burger and placing an order', () => {
+        //id ингредиентов, которые будут добавлены в конструктор
         const burgerIngredients = ['643d69a5c3f7b9001cfa093c', '643d69a5c3f7b9001cfa0941', '643d69a5c3f7b9001cfa093e', '643d69a5c3f7b9001cfa0944'];
 
+        //сборка бургера
         burgerIngredients.forEach(id => {
             cy.get(`[data-testid=cy_${id}] > button`).click();
         });
 
+        //клик по кнопке оформления заказа
         cy.get('[data-testid=cy_burgerConstructor_submitSection] > button').click();
 
+        //проверка открытия модалки
         const modal = cy.get('[data-testid=cy_modal]');
         modal.should('be.visible');
 
+        //проверка отображаемого номера заказа
         const title = cy.get('[data-testid=cy_orderDetails_title]');
         title.should('be.visible');
         title.should('have.text', '70000');
         
+        //закрытие модалки
         modal.find('[data-testid=cy_modal_closeButton]').click({ multiple: true });
         modal.should('not.exist');
 
+        //проверка, что конструктор пуст
         cy.get('[data-testid=cy_burgerConstructor_defaultBun_1]').should('be.visible');
         cy.get('[data-testid=cy_burgerConstructor_defaultIngredient]').should('be.visible');
         cy.get('[data-testid=cy_burgerConstructor_defaultBun_2]').should('be.visible');
