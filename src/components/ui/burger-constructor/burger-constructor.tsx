1568fe8c3dd5ccf4c33d69f6eda0e21969f8ef7c
Vladislav Guzenkov <vladislav.srg@yandex.ru>
2025-02-10 22:22:54 +0300

    Добавлены атрибуты data-testid для cypress

--- src/components/ui/burger-constructor/burger-constructor.tsx
+++ src/components/ui/burger-constructor/burger-constructor.tsx
@@ -73,7 +73,7 @@ export const BurgerConstructorUI: FC<BurgerConstructorUIProps> = ({
         Выберите булки
       </div>
     )}
-    <div className={`${styles.total} mt-10 mr-4`}>
+    <div data-testid='cy_burgerConstructor_submitSection' className={`${styles.total} mt-10 mr-4`}>
       <div className={`${styles.cost} mr-10`}>
         <p className={`text ${styles.text} mr-2`}>{price}</p>
         <CurrencyIcon type='primary' />
